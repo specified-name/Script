922efda94dd69a9bdbc261e0745ec0a39127c5ea
mick <87713723+specified-name@users.noreply.github.com>
2021-07-20 18:28:26 +0200

    Update remote.lua

--- remote.lua
+++ remote.lua
@@ -7,6 +7,6 @@ NotificationBindable.OnInvoke = callback
 game.StarterGui:SetCore("SendNotification", {
     Title = "whatever";
     Text = "skrrt dab";
-    Duration = "8";
+    Duration = "5";
     Callback = NotificationBindable;
 })
