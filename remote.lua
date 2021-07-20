025ab1ad982f0aacdf9cfc9da8adefe3edb931f7
mick <87713723+specified-name@users.noreply.github.com>
2021-07-20 18:25:01 +0200

    Update remote.lua

--- remote.lua
+++ remote.lua
@@ -1,20 +1,12 @@
 local function callback(Text)
- if Text == "OK" then
-  print ("test")
-elseif Text == ("TEST") then
- print ("ok")
- end
 end
-
+ 
 local NotificationBindable = Instance.new("BindableFunction")
 NotificationBindable.OnInvoke = callback
---
-game.StarterGui:SetCore("SendNotification",  {
- Title = "Title";
- Text = "Description";
- Icon = "";
- Duration = 5;
- Button1 = "OK"
- Button2 = "TEST"
- Callback = NotificationBindable;
+ 
+game.StarterGui:SetCore("SendNotification", {
+    Title = "whatever";
+    Text = "skrrt dab";
+    Duration = "8";
+    Callback = NotificationBindable;
 })
