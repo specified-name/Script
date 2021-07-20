76d8e4a7ac9c8d2dc1aefd7e21182132609136ad
mick <87713723+specified-name@users.noreply.github.com>
2021-07-20 18:33:45 +0200

    Update remote.lua

--- remote.lua
+++ remote.lua
@@ -1,12 +1,12 @@
 local function callback(Text)
 end
- 
+
 local NotificationBindable = Instance.new("BindableFunction")
 NotificationBindable.OnInvoke = callback
  
 game.StarterGui:SetCore("SendNotification", {
-    Title = "Title";
-    Text = "Description";
+    Title = "Trolled ololololololooollolaaa";
+    Text = "This does literally NOTHING, well expect displaying text";
     Duration = "5";
     Callback = NotificationBindable;
 })
